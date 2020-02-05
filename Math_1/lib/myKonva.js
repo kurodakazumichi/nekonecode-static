5de74a3e4da71db4747a96d9f7cec4a3cb740141
kazumichi kuroda <kazumichi-kuroda@nekonecode.com>
2020-02-06 04:02:44 +0900

    Refactor グラフの図形を扱いやすくするために一新

--- Math_1/lib/myKonva.js
+++ Math_1/lib/myKonva.js
@@ -44,58 +44,107 @@ const points = (list) => {
 }
 
 /******************************************************************************
+ * 形状
+ *****************************************************************************/
+/** 形状基底クラス */
+class Shape {
+  constructor() {
+    this.node = this.createNode();
+  }
+
+  /** 派生先で定義 */
+  createNode() {}
+
+  points(list) {
+    this.node.setAttr("points", points(list)); return this;
+  }
+  strokeWidth(w) {
+    this.node.setAttr("strokeWidth", w); return this;
+  }
+  dash(d) {
+    this.node.setAttr("dash", [d]); return this;
+  }
+  stroke(c) {
+    this.node.setAttr("stroke", c); return this;
+  }
+  radius(r) {
+    this.node.setAttr("radius", r); return this;
+  }
+  fill(c) {
+    this.node.setAttr("fill", c); return this;
+  }
+  x(_x) {
+    this.node.setAttr("x", x(_x)); return this;
+  }
+  y(_y) {
+    this.node.setAttr("y", y(_y)); return this;
+  }
+  xy(x, y) {
+    this.x(x).y(y); return this;
+  }
+  text(text) {
+    this.node.setAttr("text", text); return this;
+  }
+  fontSize(s) {
+    this.node.setAttr("fontSize", s); return this;
+  }
+  visible(flg) {
+    this.node.setAttr("visible", flg); return this;
+  }
+}
+
+/** Line */
+class ShapeLine extends Shape
+{
+  createNode() {
+    return new Konva.Line({
+      stroke      : "black",
+      strokeWidth : 1,
+    });
+  }
+}
+
+/** Circle */
+class ShapeCircle extends Shape {
+  createNode() {
+    return new Konva.Circle();
+  }
+}
+
+/** Text */
+class ShapeText extends Shape {
+  createNode() {
+    return new Konva.Text();
+  }
+}
+
+/******************************************************************************
  * 形状システム
  *****************************************************************************/
-class Shape {
+class ShapeSystem {
 
   axisX() {
-    return new Konva.Line({
-      points: points([c.l, 0, c.r, 0]),
-      stroke: 'black',
-      strokeWidth: 1,
-    });
+    return new ShapeLine().points([c.l, 0, c.r, 0]);
   }
 
   axisY() {
-    return new Konva.Line({
-      points: points([0, c.t, 0, c.d]),
-      stroke: 'black',
-      strokeWidth: 1,
-    });
+    return new ShapeLine().points([0, c.t, 0, c.d]);
   }
 
   solidLine(width=2) {
-    return new Konva.Line({
-      points: points([0, 0, 0, 0]),
-      stroke: 'black',
-      strokeWidth: width,
-    });
+    return new ShapeLine().strokeWidth(width);
   }
   brokenLine(width=2) {
-    return new Konva.Line({
-      points: points([0, 0, 0, 0]),
-      stroke: 'black',
-      strokeWidth: width,
-      dash:[5]
-    });
+    return new ShapeLine().strokeWidth(width).dash(5);
   }
-
-  dot1(color = "black") {
-    return new Konva.Circle({
-      radius:5,
-      fill: color,
-    })
+  dot(color = "black") {
+    return new ShapeCircle().radius(4).fill(color);
   }
-
   text(text="", size=15, color="black"){
-    return new Konva.Text({
-      text,
-      fontSize:size,
-      fill:color,
-    })
+    return new ShapeText().text(text).fontSize(size).fill(color);
   }
 }
-const sShape = new Shape();
+const sShape = new ShapeSystem();
 
 /******************************************************************************
  * グラフ
@@ -113,15 +162,15 @@ class Graph {
   }
   add(shapes) {
     Object.values(shapes).map((s) => {
-      this.layer.add(s);
+      this.layer.add(s.node);
     })
   }
   axisX() {
-    this.layer.add(sShape.axisX());
+    this.layer.add(sShape.axisX().node);
     return this;
   }
   axisY() {
-    this.layer.add(sShape.axisY());
+    this.layer.add(sShape.axisY().node);
     return this;
   }
   draw() {

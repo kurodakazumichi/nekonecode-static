53c261428723b40128d10c4cec79b394e19d6769
kazumichi kuroda <kazumichi-kuroda@nekonecode.com>
2020-02-11 13:21:13 +0900

    Added コメント

--- three/primer/01/index.js
+++ three/primer/01/index.js
@@ -1,34 +1,39 @@
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  const width = 960;
+  const width  = 960;
   const height = 540;
 
   // レンダラーを作成
   const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#myCanvas')
+    canvas: document.querySelector("#myCanvas")
   });
+
+  // デバイスピクセル比の設定
+  // これを設定しないとスマホだとぼやけて表示されます
   renderer.setPixelRatio(window.devicePixelRatio);
+
+  // レンダラーのサイズを設定する(デフォルトは小さい)
   renderer.setSize(width, height);
 
   // シーンを作成
   const scene = new THREE.Scene();
 
   // カメラを作成
-  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+  // 引数は (画角, アスペクト比, near, far)
+  const camera = new THREE.PerspectiveCamera(45, width/height, 1, 10000);
   camera.position.set(0, 0, +1000);
 
   // 箱を作成
   const geometry = new THREE.BoxGeometry(500, 500, 500);
-  const material = new THREE.MeshStandardMaterial({color: 0x0000FF});
+  const material = new THREE.MeshStandardMaterial({color:0x0000FF});
   const box = new THREE.Mesh(geometry, material);
   scene.add(box);
 
-  // 平行光源
+  // ディレクショナルライト
   const light = new THREE.DirectionalLight(0xFFFFFF);
-  light.intensity = 2; // 光の強さを倍に
+  light.intensity = 2;
   light.position.set(1, 1, 1);
-  // シーンに追加
   scene.add(light);
 
   // 初回実行
@@ -37,7 +42,6 @@ function init() {
   function tick() {
     requestAnimationFrame(tick);
 
-    // 箱を回転させる
     box.rotation.x += 0.01;
     box.rotation.y += 0.01;
 
